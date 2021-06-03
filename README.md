3505380f28e5726fc0a4e957879cea25fef76b60
waverune <yashviknaithani@gmail.com>
2021-06-03 19:27:12 +0530

    init implementation

---
+++ README.md
@@ -0,0 +1,9 @@
+Running instructions
+
+1. Install dependencies
+	i.	BeautifulSoup   		-> 'pip install beautifulsoup4'
+	ii. 	Pandas				-> 'pip install pandas'
+	
+	
+	
+2. Run 'python3 scraper.py' in the command line
---
+++ scraper.py
@@ -0,0 +1,73 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import urllib.request
+
+
+# read the html file (implement read fromn site)
+url = 'https://www.bseindia.com/markets/equity/EQReports/bulk_deals.aspx'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36'}
+
+html_text = requests.get(
+    url, headers=headers)
+
+soup = BeautifulSoup(html_text.content, 'lxml')
+
+
+# init lists
+
+Date = []
+Code = []
+SecName = []
+Name = []
+Type = []
+Quantity = []
+Price = []
+
+
+j = 0
+
+# parse the td tags in sequence
+td_tags = soup.find_all(
+    'td', class_=['tdcolumn', 'tdcolumn text-right', 'TTRow_left'])
+for info in td_tags:
+    if (j % 7) == 1:
+        Date.append(info.text)
+    if (j % 7) == 2:
+        Code.append(info.text)
+    if (j % 7) == 3:
+        SecName.append(info.text)
+    if (j % 7) == 4:
+        Name.append(info.text)
+    if (j % 7) == 5:
+        Type.append(info.text)
+    if (j % 7) == 6:
+        Quantity.append(info.text)
+    if (j % 7) == 0:
+        Price.append(info.text)
+
+    j = j+1
+
+
+print("^^^^^^^^^^^^")
+# checks len =
+print(len(Date))
+print(len(Code))
+print(len(SecName))
+print(len(Name))
+print(len(Type))
+print(len(Quantity))
+print(len(Price))
+
+df = pd.DataFrame({
+    'Deal Date': Date,
+    'Security Code': Code,
+    'Security Name': SecName,
+    'Client Name': Name,
+    'Deal Type': Type,
+    'Quantity': Quantity,
+    'Price': Price,
+})
+
+df.to_excel('EXCELLL.xlsx', index=False)
